a9d977e34afd8986e471f3b694f5b90faac960ca
Moksleivis <Moksleivis@kitm.local>
2023-04-04 14:32:23 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let  myArray = ["JS23s", "JS23T", "t23"];
+console.log(myArray);
+console.log(myArray[2]);
+console.log(myArray.length);
+
+// myArray.push("A23s");
+// console.log(myArray);
+
+// myArray.pop();
+// console.log(myArray);
+
+// myArray.shift();
+// console.log(myArray);
+
+// myArray.unshift("JJ23s");
+// console.log(myArray);
+
+// myArray.splice(1, 2);
+// console.log(myArray);
+
+// myArray.forEach((produktas, index) => {console.log(index +1, produktas)})
+// con
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// for(let i = 0; i < 10; i--) {
+//     console.log(i);
+// }
+
+// for(let i = 0; i < 10; i--) {
+//     prompt(i);
+// }
